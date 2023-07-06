c6eb66441c9da155999fe7dc0c67d9dd4c087354
qiyas khan <qiyaskhan2@gmail.com>
2023-07-06 17:08:20 +0500

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "QIYAS";
+const meta = {
+    title: "Qiyas khan",
+    description: "I have 1 year of experience as a React JS and React Native developer, with expertise in building dynamic and responsive user interfaces for web and mobile applications,currently working in Pakistan",
+};
+
+const introdata = {
+    title: "I’m Qiyas khan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites And Mobile APP",
+        third: "I develop mobile apps And website using React",
+    },
+    description: "I have 1 year of experience as a React JS and React Native developer, with expertise in building dynamic and responsive user interfaces for web and mobile applications.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit About My self",
+    aboutme: "I have 1 year of experience as a React JS and React Native developer, with expertise in building dynamic and responsive user interfaces for web and mobile applications.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "Eragon",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of month",
+        where: "Netware Solution",
+        date: "2022",
+    },
+    {
+        jobtitle: "Designer of year",
+        where: "Netware Solution",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "React Native",
+        value: 90,
+    },
+    {
+        name: "React Js",
+        value: 88,
+    },
+    {
+        name: "Javascript",
+        value: 88,
+    },
+    {
+        name: "BootStrip",
+        value: 85,
+    },
+    {
+        name: "Tailwind",
+        value: 85,
+    },
+    {
+        name: "Redux",
+        value: 83,
+    },
+];
+
+const services = [{
+        title: "React Native",
+        description: "If you are looking to build a mobile app,using React Native we can help you to create high-performance, native mobile apps that reach a wider audience.",
+    },
+    {
+        title: "React js",
+        description: "if you are for a good and user friendly website using react js we can offers you a bitter service",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Custom WordPress theme developmen Plugin development Website customization.Website maintenance and support:",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Qiyaskhan2@gmail.com",
+    YOUR_FONE: "(+92)3069168467",
+    description: "I have 1 year of experience as a React JS and React Native developer, with expertise in building dynamic and responsive user interfaces for web and mobile applications",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Qiyaskhan",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/feed/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
